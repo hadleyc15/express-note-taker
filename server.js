7dcc3d70ef28efa6fee37ed7fed7dcf82b5db86e
Chris Hadley <hadleyc15@yahoo.com>
2020-07-12 12:53:45 -0600

    initial setup to server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const PORT = 3001;
-
+const htmlRoute = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoutes');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//calls routes
+app.use(express.static("public"));
+app.use('/', htmlRoute);
+app.use('/', apiRoute);
 
-
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
