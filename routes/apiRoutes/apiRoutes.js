9591ba5a90907731f27e8f5c3db1dd6892ab03da
Chris Hadley <hadleyc15@yahoo.com>
2020-07-14 16:48:29 -0600

    updates to make it all work

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,1 +1,65 @@
-//Not exactly sure what sould be put here......
+const router = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/notes', (req, res) => {
+    //looks for any previous notes
+    
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+
+        let noteData = [];
+        if (err) {
+            throw err;
+        }
+        if (data.length > 2) {
+            noteData = JSON.parse(data);
+            res.send(noteData);
+        }
+        //if no previous notes are found, then this
+        else {
+            console.log('No notes saved');
+        }
+    })
+});
+
+//create new Note
+router.post('/notes', (req, res) => {
+
+    let newNote = {
+        title: req.body.title, 
+        text: req.body.text,
+        id: uuidv4(),
+    };
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(`err at the database ${err}`);
+        }
+
+        else if (data.length > 2) {
+            obj = JSON.parse(data);
+            obj.push(newNote);
+            //adds the new note to the list on the left
+            fs.writeFile('db/db.json', JSON.stringify(obj), 'utf8', (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log('Note saved.')
+                res.json(obj)
+            });
+        }
+        else {
+            obj = [];
+            obj.push(newNote);
+            fs.writeFile('db/db.json', JSON.stringify(obj), 'utf8', (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log('Note saved.')
+                res.json(obj)
+            });
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
 const app = express();
-const PORT = 3001;
-const htmlRoute = require('./routes/htmlRoutes/htmlRoutes');
-//const apiRoute = require('./routes/apiRoutes');
+const PORT = process.env.PORT || 3001;
+const htmlRoute = require('./routes/htmlRoutes/htmlRoutes.js');
+const apiRoute = require('./routes/apiRoutes/apiRoutes.js');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,71 +10,12 @@ app.use(express.json());
 
 //calls routes
 app.use(express.static("public"));
+app.use('/api', apiRoute);
 app.use('/', htmlRoute);
-
-//app.use('/', apiRoute);
-
-//display notes
-app.get('/api/notes', (req,res) => {
-  //looks for any previous notes
-  fs.readFile('./db/db.json', 'utf8', function(err, data) {
-      let noteData = [];
-      if(err) {
-          throw err;
-      }
-      if(data.length > 2) {
-          noteData = JSON.parse(data);
-          res.send(noteData);
-      }
-      //if no previous notes are found, then this
-      else {
-          console.log('No notes saved');
-      }
-  })   
-});
-
-
-//create new Note
-app.post('/api/notes', (req, res) => {
-
-let newNote = req.body;
-  
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-          console.log(`err at the database ${err}`);
-      } 
-      
-      else if (data.length > 2) {
-          obj = JSON.parse(data);
-          obj.push(newNote);
-          //adds the new note to the list on the left
-          fs.writeFile('./db/db.json', JSON.stringify(obj), 'utf8', (err) => {
-              if(err) {
-                  throw err;
-              }
-              console.log('Note saved.')
-          });
-     }
-      else {
-          obj = [];
-          obj.push(newNote);
-          fs.writeFile('./db/db.json', JSON.stringify(obj), 'utf8', (err) => {
-              if(err) {
-                  throw err;
-              }
-              console.log('Note saved.')
-          });
-      }
-  });
-});
 
 //BONUS CODE
 //ADD CODE HERE TO GET NOTES TO DELETE BY PUSHING THE TRASH CAN ICON.....
 
-
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname,'./public/index.html'));
-});
 
 //Listens and sends the data to a specific port
 app.listen(PORT, () => {
